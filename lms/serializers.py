3fa44eca962e04e67c6776ded1de8c5f7ddfd7ac
ArtemMTim <artemtimofeev@rambler.ru>
2024-12-04 18:30:57 +0300

    lessons_total for course

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,10 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lms.models import Course, Lesson
 
 class CourseSerializer(ModelSerializer):
+    lessons_total = SerializerMethodField()
+
+    def get_lessons_total(self, course):
+        return course.lesson_set.count()
     class Meta:
         model = Course
-        fields = ('id', 'title', 'preview', 'description')
+        fields = ('id', 'title', 'preview', 'description', 'lessons_total')
 
 class LessonSerializer(ModelSerializer):
     class Meta:
