11e599c800a55262b790bc4065713fef376ef636
ArtemMTim <artemtimofeev@rambler.ru>
2024-12-05 18:28:41 +0300

    Вывод информации об уроках в курсе

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,13 +4,18 @@ from lms.models import Course, Lesson
 
 class CourseSerializer(ModelSerializer):
     lessons_total = SerializerMethodField()
+    lessons = SerializerMethodField()
+
+    def get_lessons(self, course):
+        #return [f'{lesson.title} - {lesson.description}' for lesson in Lesson.objects.filter(course=course)]
+        return [f'{lesson.title} - {lesson.description}' for lesson in course.lesson_set.all()]
 
     def get_lessons_total(self, course):
         return course.lesson_set.count()
 
     class Meta:
         model = Course
-        fields = ("id", "title", "preview", "description", "lessons_total")
+        fields = ("id", "title", "preview", "description", "lessons_total", "lessons")
 
 
 class LessonSerializer(ModelSerializer):
